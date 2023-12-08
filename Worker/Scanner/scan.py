b708e51f6fdfe05d11c03096f9bb9e30995b5fe9
capitan <capitansec@gmail.com>
2023-12-08 14:26:52 +0300

    DocString for PEP8 added port scanner

--- Worker/Scanner/scan.py
+++ Worker/Scanner/scan.py
@@ -1,23 +1,21 @@
 import socket
 
 def port_scan(host, mode):
-    # Ordinal attributes for scan mode -> 1: first 1024, 2: 65535
-    # Scan types(TCP UDP etc.) not implemented yet.
-
+    """"
+    Scan ports of host
+    :param host: strign, the host to scan
+    :param mode: int, the mode to scan
+    """
+    mode += 1
     try:
-
         for port in range(1, 65535):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(1)
-
-            # returns an error indicator
-            result = s.connect_ex((host, port))
+            result = sconn.connect_ex((host, port))
             if result == 0:
 
                 # Should index on elasticsearch
-                print("Port {} is open".format(port))
-            s.close()
-
+                print(f"Port {port} is open")  # Will be deleted
+            sconn.close()
     except socket.gaierror:
         print("\n Hostname Could Not Be Resolved !!!!")
-
