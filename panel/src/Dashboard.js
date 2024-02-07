9c6375726020014230b821133c164f5355e0fb0d
capitansec <capitansec@gmail.com>
2024-02-07 10:24:07 +0300

    Scan results and panel logout, scan notifications

--- panel/src/Dashboard.js
+++ panel/src/Dashboard.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, Dropdown, Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import './Dashboard.css';
 import Users from './components/dashboard/Users';
-
+import Scans from './components/dashboard/Scans';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
 
 const Dashboard = () => {
-  const handleLogout = () => {
-    sessionStorage.removeItem()
-    console.log("Logout");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [activeComponent, setActiveComponent] = useState('scans'); // Default to 'scans'
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/v1/user/logout', {
+        headers: {
+          'accept': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      });
+  
+      if (response.data.status === 'success') {
+        console.log('User logged out successfully');
+      } else {
+        console.error('Error logging out:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  
+    sessionStorage.removeItem('token');
+    dispatch({ type: 'LOGOUT' });
+    navigate("/dashboard");
   };
 
   const menu = (
@@ -21,10 +46,16 @@ const Dashboard = () => {
 
   return (
     <div className='wrapper2'>
-      <Menu mode="horizontal" theme="dark">
-        <Menu.Item key="1"><h3>Scans</h3></Menu.Item>
-        <Menu.Item key="2"><h3>Status</h3></Menu.Item>
-        <Menu.Item key="3"><h3>Users</h3></Menu.Item>
+      <Menu mode="horizontal" theme="dark" style={{marginBottom: '60px'}}>
+        <Menu.Item key="1" onClick={() => setActiveComponent('scans')}>
+          <h3>Scans</h3>
+        </Menu.Item>
+        <Menu.Item key="2" onClick={() => setActiveComponent('users')}>
+          <h3>Users</h3>
+        </Menu.Item>
+        <Menu.Item key="3" onClick={() => setActiveComponent('status')}>
+          <h3>Status</h3>
+        </Menu.Item>
         <Menu.Item id='tester' key="4" style={{ float: 'right' }}>
           <Dropdown overlay={menu}>
             <Avatar size={64} icon={<UserOutlined />} style={{ fontSize: '18px', fontWeight: 'bold' }} />
@@ -32,9 +63,9 @@ const Dashboard = () => {
         </Menu.Item>
       </Menu>
 
-      <Users />
-
-
+      {activeComponent === 'scans' && <Scans />}
+      {activeComponent === 'status' && <Scans />}
+      {activeComponent === 'users' && <Users />}
     </div>
   );
 };
--- panel/src/components/dashboard/Scans.js
+++ panel/src/components/dashboard/Scans.js
@@ -1,144 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Scans.css';
-import { Button } from 'antd';
-import { Space, Table, Tag } from 'antd';
+import { Button, Input, Table, message } from 'antd'; 
+import axios from 'axios';
 import { Pagination } from 'antd';
 
+const pageSize = 7;
 
-const pageSize = 7
-const columns = [
+const Scans = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [scanResults, setScanResults] = useState([]);
+
+  const handleScan = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/v1/scan', {
+        target: searchTerm, 
+        comment: 'string',
+      }, {
+        headers: {
+          'accept': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.data === 'İletildi') {
+        message.success('Scan request has been successfully sent.');
+        setSearchTerm(''); 
+      }
+    } catch (error) {
+      console.error('Error sending scan request:', error);
+      message.error('Error sending scan request. Please try again.'); 
+    }
+  };
+
+  const handleScanResults = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/v1/result', {
+        headers: {
+          'accept': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
+        },
+      });
+
+      if (response.data.results) {
+        setScanResults(response.data.results);
+      }
+    } catch (error) {
+      console.error('Error fetching scan results:', error);
+    }
+  };
+
+  const columns = [
     {
-      title: 'Host',
-      dataIndex: 'name',
-      key: 'name',
-      render: (text) => <a>{text}</a>,
+      title: 'IP',
+      dataIndex: 'ip',
+      key: 'ip',
     },
     {
       title: 'Port',
-      dataIndex: 'age',
-      key: 'age',
+      dataIndex: 'port',
+      key: 'port',
     },
     {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-          <>
-            {tags.map((tag) => {
-              let color = tag.length > 5 ? 'geekblue' : 'green';
-              if (tag === 'loser') {
-                color = 'volcano';
-              }
-              return (
-                <Tag color={color} key={tag}>
-                  {tag.toUpperCase()}
-                </Tag>
-              );
-            })}
-          </>
-        ),
-      },
-    {
-      title: 'Datetime',
-      dataIndex: 'address',
-      key: 'address',
+      title: 'State',
+      dataIndex: 'state',
+      key: 'state',
     },
-    
-    
+    {
+      title: 'Timestamp',
+      dataIndex: '@timestamp',
+      key: '@timestamp',
+    },
   ];
 
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-    {
-        key: '4',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-      },
-      {
-        key: '5',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-      },
-      {
-        key: '6',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-      },
-      {
-        key: '7',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-      },
-      {
-        key: '8',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-      },
-  ];
+  return (
+    <>
+      <div className='base'>
+        <div className='input-box' >
+          <input style={{marginRight: '20px'}}
+            type="text"
+            placeholder='IP or Hostname'
+            id='ip-box'
+            onChange={(e) => setSearchTerm(e.target.value)} 
+            value={searchTerm} 
+            required
+          />
+          <Button type="primary" danger style={{marginRight: '20px'}} onClick={handleScan}>
+            Scan
+          </Button>
 
+          <Button type="primary" onClick={handleScanResults}>
+            Refresh
+          </Button>
+        </div>
 
-
-const Scans = () => {
-
-    return (
-        <>
-            <div className='base'>
-                <div className='input-box'>
-                    <input
-                        type="text"
-                        placeholder='IP or Hostname'
-                        id='ip-box'
-                        required
-                    />
-                    <Button type="primary" danger>
-                        Scan
-                    </Button>
-
-                </div>
-
-                <div className='table-content'>
-                <Table columns={columns} dataSource={data} pagination={{ pageSize }} />
-            </div>
-            
-            </div>
-        </>
-    );
-
+        <div className='table-content'>
+          <Table columns={columns} dataSource={scanResults} pagination={{ pageSize }} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Scans;
-
-
-
-
-
